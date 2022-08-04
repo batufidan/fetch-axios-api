5351f59eef56e6e4294c22d8492cc45d3c2c898b
unknown <batuhan.fidan@ogr.iu.edu.tr>
2022-08-04 17:58:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
+
+
+// export default  function App() {
+
+  // const [countries, setCountries] = useState([]);
+
+  // Fetch api ile bağlanma
+  
+//  useEffect(() => {
+//    fetch("https://restcountries.com/v2/all")
+//    .then(response => response.json())
+//    .then(response => setCountries(response));
+//    .catch(error => console.log({error}));
+//  }, []);
+
+
+
+
+// Axios ile bağlanma (npm i axios)
+
+//  useEffect(() => {
+//    axios.get('https://restcountries.com/v2/all')
+//    .then(response => setCountries(response.data));
+//  }, []);
+
+//  return (
+//    <div className="App">
+//       <h1>Hello Batuhan</h1>
+
+//       {countries.map(country => {
+//         return (
+//           <div key={country.name}>
+//             <h2>{country.name}</h2>
+//             <h4>{country.capital}</h4>
+//             <p>
+//               <img src={country.flag} alt={country.name} style={{width: "100px"}} />
+//             </p>
+//           </div>
+//         );
+//       })}
+//      </div>
+//    );
+//  }
+
+
+
+
+
+import { useEffect, useState } from "react";
+import React from "react";
 
 function App() {
+
+  const [users, setUsers] = useState(false)
+
+
+  useEffect(() => {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+      if (res.ok && res.status === 200) {
+        return res.json()
+      }
+    })
+    .then(data => setUsers(data))
+    .catch(err => console.log(err))
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {users && users.map(user => (
+        <li key={user.id}>
+          {user.name}
+        </li>
+      ))}
+    </ul>
   );
 }
 
+
 export default App;
+
+
+
+
+
+
+
+
+
